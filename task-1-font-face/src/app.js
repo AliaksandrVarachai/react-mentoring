ac54851f390452b6f02bc9c285783cf6e3400dda
Aliaksnadr Varachai <Aliaksandr_Varachai@epam.com>
2016-12-20 20:42:16 +0300

    font is loaded with css-loader

---
+++ task-1-font-face/src/app.js
@@ -0,0 +1,9 @@
+require('./styles.css');
+
+document.onload = function() {
+    console.log('js is loaded');
+};
+
+document.getElementById('go').onclick = function() {
+    console.log('hi');
+};
---
+++ task-1-font-face/webpack.config.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var SRC_DIR = path.resolve(__dirname, 'src');
+var DIST_DIR = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    entry: {
+        'app': path.resolve(SRC_DIR, 'app')
+    },
+    output: {
+        path: DIST_DIR,
+        filename: '[name].js',
+        publicPath: '/'
+    },
+    watch: true,  //TODO: make prod version
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new ExtractTextPlugin('styles.css', {allChunks: true})
+    ],
+    module: {
+        loaders: [
+            {
+                test: /(\.jsx?$)|(\.css)/,
+                loader: 'react-hot',
+                exclude: /node_modules/
+            }, {
+
+                test: /\.css$/,
+                include: SRC_DIR,
+                loader: 'style-loader!css-loader'
+            }, {
+                test: /\.woff/,
+                include: path.resolve(SRC_DIR, 'fonts'),
+                loader: 'url-loader?limit=10000'
+            }, {
+                test: /\.jsx?$/,
+                include: SRC_DIR,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
