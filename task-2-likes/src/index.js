bcf9b6f99665cd128e39d72ef679938e3e50fca4
Aliaksnadr Varachai <Aliaksandr_Varachai@epam.com>
2017-01-25 12:58:00 +0300

    draft redux

--- task-2-likes/src/index.js
+++ task-2-likes/src/index.js
@@ -2,24 +2,78 @@ import './styles.css';
 import 'file!../index.html';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers} from 'redux';
+import {createStore} from 'redux';
 import {connect} from 'react-redux';
 
 import Item from './components/item';
 
 console.log('index.js is loaded');
 
+let data = {
+    "author": "Alex",
+    "items":
+    [
+        {
+            "name": "Stan Marsh",
+            "avatarSrc": "http://www.southparkfan.ru/wall/images_large/avatars/001988.jpg",
+            "greeting": "Oh my God, they killed Kenny!",
+            "sayHello": false
+        }, {
+        "name": "Kyle Broflovski",
+        "avatarSrc": "http://www.southparkfan.ru/wall/images_large/avatars/002004.jpg",
+        "greeting": "You bastards!",
+        "sayHello": false
+    }, {
+        "name": "Eric Cartman",
+        "avatarSrc": "http://www.southparkfan.ru/wall/images_large/avatars/001980.jpg",
+        "greeting": "Screw you guys, I'm going home.",
+        "sayHello": false
+    }, {
+        "name": "Kenny McCormick",
+        "avatarSrc": "http://www.southparkfan.ru/wall/images_large/avatars/001991.jpg",
+        "greeting": "Bo boo bo boo bo booboo bob boo:)",
+        "sayHello": false
+    }, {
+        "name": "Leopold Stotch",
+        "avatarSrc": "http://www.southparkfan.ru/wall/images_large/avatars/002015.jpg",
+        "greeting": "Hi! My name is Butters!",
+        "sayHello": false
+    }, {
+        "name": "Wendy Testaburger",
+        "avatarSrc": "http://www.southparkfan.ru/wall/images_large/avatars/001999.jpg",
+        "greeting": "Glad to see you!",
+        "sayHello": false
+    }
+    ]
+};
+
+let reducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return data;
+        case 'SAY_HELLO':
+            //TODO: add logic for checkbox
+            return data;
+        default:
+            return data;
+    }
+};
+
 class App extends React.Component {
     constructor() {
         super();
-        this. state = {};
+        this.state = {}; //for any case
     }
 
     componentDidMount() {
+        const {store} = this.props;
+        this.unsubscribe = store.subscribe(() => this.forceUpdate());
+
         fetch("https://dl.dropboxusercontent.com/s/u7bx7ffexwuqtqc/init.json")
             .then(response => response.json())
             .then(data => {
                 console.dir(data.items);
+                //store.dispatch({type: 'null'});
                 this.setState({items: data.items})
             })
             .catch(error => {
@@ -27,10 +81,16 @@ class App extends React.Component {
             });
     }
 
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+
     render() {
+        const {store} = this.props;
+        const state = store.getState();
         return (
-            <div>
-                {this.state.items ? this.state.items.map(item => (
+            <Provider store={createStore(reducer)}>
+                {state.items ? state.items.map(item => (
                     <Item
                         name={item.name}
                         avatarSrc = {item.avatarSrc}
@@ -38,9 +98,15 @@ class App extends React.Component {
                         sayHello={item.sayHello}
                     />
                 )) : ""}
-            </div>
+            </Provider>
         )
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app-container'));
+class Provider extends React.Component {
+    render() {
+        return this.props.children;
+    }
+}
+
+ReactDOM.render(<App store={createStore(reducer)}/>, document.getElementById('app-container'));
